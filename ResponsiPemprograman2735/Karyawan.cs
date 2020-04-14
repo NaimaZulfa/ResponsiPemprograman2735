e379cd0cc7c9fca587edbe7f68d0241f7b795066
Naima Zulfa <naima.zulfa@students.amikom.ac.id>
2020-04-14 13:44:59 +0700

    inisialisasi project

---
+++ ResponsiPemprograman2735/Karyawan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemprograman2735
+{
+    class Karyawan
+    {
+        public int No { get; set; }
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int NaikGaji { get; set; }
+
+        public Karyawan(int no, int nik, string nama, int gajibulanan)
+        {
+            this.No = no;
+            this.Nik = nik;
+            this.Nama = nama;
+
+            if (gajibulanan < 0)
+            {
+                GajiBulanan = 0;
+                NaikGaji = Convert.ToInt32((GajiBulanan * 0.1 + GajiBulanan));
+            }
+            else
+            {
+                this.GajiBulanan = gajibulanan;
+                NaikGaji = Convert.ToInt32((GajiBulanan * 0.1 + GajiBulanan));
+            }
+        }
+    }
+}
---
+++ ResponsiPemprograman2735/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ResponsiPemprograman2735
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t");
+            Karyawan karyawan1 = new Karyawan(1 , 1901, "Dinda", 3000000);
+            Karyawan karyawan2 = new Karyawan(2 , 1905, "Naima", 2000000);
+
+            Console.WriteLine("\t");
+            Console.WriteLine(" No  Nik/Nama\t\t  Gaji Bulanan\t ");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine(" {0}  {1} {2} \t\t  {3}\t ", karyawan1.No, karyawan1.Nik, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine(" {0}  {1} {2} \t\t  {3}\t ", karyawan2.No, karyawan2.Nik, karyawan2.Nama, karyawan2.GajiBulanan);
+
+            Console.WriteLine("\n\nAsiiiiiik kenaikan gaji 10%!!\n");
+
+            Console.WriteLine("\t");
+            Console.WriteLine(" No  Nik/Nama\t\t  Gaji Bulanan\t ");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine(" {0}  {1} {2} \t\t  {3}\t ", karyawan1.No, karyawan1.Nik, karyawan1.Nama, karyawan1.NaikGaji);
+            Console.WriteLine(" {0}  {1} {2} \t\t  {3}\t ", karyawan2.No, karyawan2.Nik, karyawan2.Nama, karyawan2.NaikGaji);
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
